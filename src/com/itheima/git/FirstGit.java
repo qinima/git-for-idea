98f1ac7638bd8bb04178b125affdc464e5c60f35
zhangsan <qinima@163.com>
2018-11-18 15:53:27 +0800

    本地工程添加到git

---
+++ src/com/itheima/git/FirstGit.java
@@ -0,0 +1,10 @@
+package com.itheima.git;
+
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+        System.out.println("hello git!");
+    }
+}
